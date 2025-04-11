de5b8a995ef4a2c8a5f3dafd4a9025c37392d8b8
whoami847 <mrbeastbangla069@gmail.com>
2025-04-11 19:25:48 +0600

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -1,11 +1,39 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URI
 
+# Connect to MongoDB
 client = AsyncIOMotorClient(MONGO_URI)
 db = client["file_share_bot"]
 
+# Collections
 users_col = db["users"]
 files_col = db["files"]
 settings_col = db["settings"]
 batches_col = db["batches"]
 broadcast_col = db["broadcast"]
+force_sub_col = db["force_sub"]  # Collection for force subscription
+
+# Force Subscription Functions
+
+async def get_fsub_channels():
+    # Fetching all force subscription channels from the database
+    data = await force_sub_col.find().to_list(length=None)
+    return [item["chat_id"] for item in data]  # Return a list of chat_ids
+
+async def add_fsub_channel(chat_id):
+    # Adding a new channel to the force subscription list
+    exists = await force_sub_col.find_one({"chat_id": chat_id})
+    if not exists:
+        await force_sub_col.insert_one({"chat_id": chat_id})
+
+async def remove_fsub_channel(chat_id):
+    # Removing a channel from the force subscription list
+    await force_sub_col.delete_one({"chat_id": chat_id})
+
+async def is_user_joined(client, user_id, channel_id):
+    # Check if the user has joined the given channel
+    try:
+        member = await client.get_chat_member(channel_id, user_id)
+        return member.status in ["member", "administrator", "creator"]
+    except Exception:
+        return False
