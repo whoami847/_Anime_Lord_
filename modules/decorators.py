f70d26b46854d75cbd8b07719de9ee8dd9944264
whoami847 <mrbeastbangla069@gmail.com>
2025-04-11 19:40:20 +0600

    Update decorators.py

--- modules/decorators.py
+++ modules/decorators.py
@@ -1,12 +1,19 @@
-from pyrogram.types import Message
-from pyrogram import Client, filters
-from config import ADMINS
 from functools import wraps
+from pyrogram.types import Message
+from config import ADMINS
 
+# Admin Only Decorator
 def admin_only(func):
     @wraps(func)
-    async def wrapper(client: Client, message: Message):
+    async def wrapper(client, message: Message):
         if str(message.from_user.id) not in ADMINS:
             return await message.reply_text("🚫 You are not authorized to use this command.")
         return await func(client, message)
     return wrapper
+
+# is_admin function (for broadcast command check)
+def is_admin(user_id):
+    """Check if the user is an admin."""
+    return str(user_id) in ADMINS
+
+# Other decorators if necessary can be added here
